8445c6f804435ec675ca672b2579a4b0cce88de6
Darlinkster <Darlinkster@users.noreply.github.com>
2020-04-16 16:21:08 +0200

    Change token time

--- src/Provider/Apple.php
+++ src/Provider/Apple.php
@@ -218,7 +218,7 @@ class Apple extends AbstractProvider
             ->issuedBy($this->teamId)
             ->permittedFor('https://appleid.apple.com')
             ->issuedAt($time)
-            ->expiresAt($time + 86400*180)
+            ->expiresAt($time + 600)
             ->relatedTo($this->clientId)
             ->withClaim('sub', $this->clientId)
             ->withHeader('alg', 'ES256')
